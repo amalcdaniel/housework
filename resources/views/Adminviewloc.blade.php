d9eb1d0b9f36d5f38c63368fb542993d7bd824af
amalcdaniel <79559557+amalcdaniel@users.noreply.github.com>
2021-05-22 17:00:53 +0530

    Project completed

--- resources/views/Adminviewloc.blade.php
+++ resources/views/Adminviewloc.blade.php
@@ -284,6 +284,28 @@ table.table .avatar {
 							 <a href="#deleteEmployeeModal" class="btn btn-dark"  data-toggle="modal"><span>Delete</span></a>	
                       			</td>
                         </tr>
+						<!-- Delete Modal HTML -->
+<div id="deleteEmployeeModal" class="modal fade">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<form>
+				<div class="modal-header">						
+					<h4 class="modal-title">Delete Location</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				</div>
+				<div class="modal-body">					
+					<p>Are you sure you want to delete these Records?</p>
+					<p class="text-warning"><small>This action cannot be undone.</small></p>
+				</div>
+				<div class="modal-footer">
+					<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+					<a class="btn btn-danger" href={{"/deletelocation/".$location->id}}> <span>Delete</span></a>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
                         @endforeach
 				</tbody>
 			</table>
@@ -323,27 +345,6 @@ table.table .avatar {
 
 
 
-<!-- Delete Modal HTML -->
-<div id="deleteEmployeeModal" class="modal fade">
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<form>
-				<div class="modal-header">						
-					<h4 class="modal-title">Delete Location</h4>
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-				</div>
-				<div class="modal-body">					
-					<p>Are you sure you want to delete these Records?</p>
-					<p class="text-warning"><small>This action cannot be undone.</small></p>
-				</div>
-				<div class="modal-footer">
-					<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-					<a class="btn btn-danger" href={{"/deletelocation/".$locationview->id}}> <span>Delete</span></a>
-				</div>
-			</form>
-		</div>
-	</div>
-</div>
 
 
 <script type="text/javascript" src="src/jspdf.min.js"></script>
